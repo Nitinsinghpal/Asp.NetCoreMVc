43d409825bdeedfd1d8b1cf8dc9f06b0568a1c2b
NITTIN <nitinsingh919293@gmail.com>
2022-05-17 23:15:59 +0530

    add Mvc to application

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,17 +13,19 @@ namespace BookStore
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        ////public Startup(IConfiguration configuration)
+        ////{
+        ////    Configuration = configuration;
+        ////}
 
-        public IConfiguration Configuration { get; }
+        ////public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
+
+            ////services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,21 +35,48 @@ namespace BookStore
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
 
-            app.UseStaticFiles();
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from first middleware");
+            //    await next();
+            //});
+
+            ////else
+            ////{
+            ////    app.UseExceptionHandler("/Error");
+            ////}
+
+            ////app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //////app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World");
+            //    });
+            //});
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
             });
+
+
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/Home", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello From Home");
+            //    });
+            //});
+
         }
     }
 }
